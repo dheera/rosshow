b6d87e3ff5338eb59c531957e97adcce78906dc0
dheera <dheera@dheera.net>
2017-05-08 17:16:32 +0000

    new version

--- viewers/TermGraphics.py
+++ viewers/TermGraphics.py
@@ -1,14 +1,40 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import math
 import os
 import sys
 import time
 
+if sys.version_info >= (3,):
+    unichr = chr
+
+COLOR_BLACK = 0
+COLOR_RED = 1
+COLOR_GREEN = 2
+COLOR_YELLOW = 3
+COLOR_BLUE = 4
+COLOR_MAGENTA = 5
+COLOR_CYAN = 6
+COLOR_WHITE = 7
+
 MODE_BRAILLE = 0
 MODE_EASCII = 2
 
 TABLE_EASCII = " '-'.*.|'~/~/F//-\\-~/>-&'\"\"\"/)//.\\\\\\_LLL'\"<C-=CC:\\-\\vD=D|Y|Y|)AH.!i!.ii|/\"/F/Fff//rkfPrkJJ/P/P/P//>brr>kl>&&*=fF/)vb/PPDJ)19/2/R.\\\\\\\\\\\\(=T([(((C=3-5cSct!919|7Ce,\\\\\\_\\\\\\i919i9(C|)\\\\+tv\\|719|7@9_L=L_LLL_=6[CEC[=;==c2ctJ]d=¿Z6E/\\;bsbsbj]SSd=66jj]bddsbJ]j]d]d8"
+
+UNICODE_BRAILLE_MAP= [ \
+    0b00000001,
+    0b00001000,
+    0b00000010,
+    0b00010000,
+    0b00000100,
+    0b00100000,
+    0b01000000,
+    0b10000000,
+]
 
 class TermGraphics(object):
     def __init__(self, mode = MODE_BRAILLE):
@@ -18,23 +44,18 @@ class TermGraphics(object):
         self.term_shape = list(reversed(list(map(lambda x: int(x), os.popen('stty size', 'r').read().split()))))
         self.shape = (self.term_shape[0]*2, self.term_shape[1]*4)
         self.buffer = bytearray(self.term_shape[0]*self.term_shape[1])
+        self.colors = bytearray(self.term_shape[0]*self.term_shape[1])
+        self.current_color = 7
         self.mode = mode
-        self.unicode_braille_map = [ \
-          0b00000001,
-          0b00001000,
-          0b00000010,
-          0b00010000,
-          0b00000100,
-          0b00100000,
-          0b01000000,
-          0b10000000,
-        ]
 
     def clear(self):
         """
         Clear the graphics buffer.
         """
         self.buffer = bytearray(self.term_shape[0]*self.term_shape[1])
+
+    def set_color(self, color):
+        self.current_color = color
 
     def points(self, points):
         """
@@ -48,14 +69,59 @@ class TermGraphics(object):
         Draw a point at points = (x,y) where x is the column number, y is the row number, and (0,0)
         is the top-left corner of the screen.
         """
+        if type(point[0]) is not int or type(point[1]) is not int:
+            point = (int(point[0]), int(point[1]))
+
         if point[0] >= 0 and point[1] >= 0 and point[0] < self.shape[0] and point[1] < self.shape[1]:
             index = ((point[0] >> 1) + (point[1] >> 2) * self.term_shape[0])
             self.buffer[index] = self.buffer[index] | \
-              self.unicode_braille_map[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
+              UNICODE_BRAILLE_MAP[(point[0] & 0b1) | ((point[1] & 0b11) << 1)]
+            self.colors[index] = self.current_color
+
+    def poly(self, points):
+        """
+        Draws lines between a list of points = [(x0,y0), (x1,y1), (x2,y2), ...].
+        """
+        for i in range(1, len(points)):
+            self.line(points[i-1], points[i])
 
     def line(self, point0, point1):
-        print("Not yet supported")
+        """
+        Draw a line between point0 = (x0, y0) and point1 = (x1, y1).
+        """
+        if point1[0] == point0[0]:
+            if point0[1] < point1[1]:
+                for y in range(point0[1], point1[1]):
+                    self.point((point0[0], y))
+            else:
+                for y in range(point1[1], point0[1]):
+                    self.point((point0[0], y))
+            return
+        slope = (point1[1] - point0[1]) / (point1[0] - point0[0])
+        if abs(slope) <= 1:
+            if point0[0] < point1[0]:
+                for x in range(point0[0], point1[0]):
+                     self.point((x, point0[1] + slope*(x - point0[0])))
+            else:
+                for x in range(point1[0], point0[0]):
+                     self.point((x, point0[1] + slope*(x - point0[0])))
+        else:
+            if point0[1] < point1[1]:
+                for y in range(point0[1], point1[1]):
+                     self.point((point0[0] + (y - point0[1])/slope, y))
+            else:
+                for y in range(point1[1], point0[1]):
+                     self.point((point0[0] + (y - point0[1])/slope, y))
         return
+
+    def rect(self, point0, point1):
+        """
+        Draw a rectangle between corners point0 = (x0, y0) and point1 = (x1, y1).
+        """
+        self.line((point0[0], point0[1]), (point0[0], point1[1]))
+        self.line((point0[0], point1[1]), (point1[0], point1[1]))
+        self.line((point1[0], point1[1]), (point1[0], point0[1]))
+        self.line((point1[0], point0[1]), (point0[0], point0[1]))
 
     def draw(self):
         """
@@ -63,13 +129,21 @@ class TermGraphics(object):
         """
         sys.stdout.write("\033[H")
 
+        current_draw_color = -1
+
         for j in range(self.term_shape[1]):
             sys.stdout.write("\033[" + str(j+1) + ";1H")
+
             for i in range(self.term_shape[0]):
+                index = i + j*self.term_shape[0]
+                if self.colors[index] != current_draw_color:
+                    current_draw_color = self.colors[index]
+                    sys.stdout.write("\033[3" + str(current_draw_color) + "m")
                 if self.mode == MODE_BRAILLE:
-                    sys.stdout.write(chr(0x2800 + self.buffer[i + j*self.term_shape[0]]))
+                    sys.stdout.write(unichr(0x2800 + self.buffer[index]))
                 elif self.mode == MODE_EASCII:
-                    sys.stdout.write(TABLE_EASCII[self.buffer[i + j*self.term_shape[0]]])
+                    sys.stdout.write(TABLE_EASCII[self.buffer[index]])
+        sys.stdout.write("\033[37m")
         sys.stdout.flush()
 
 
