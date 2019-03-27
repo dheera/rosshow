421937690d03854350c2d478427ef7678e6f8974
dheera <dheera@dheera.net>
2019-03-27 06:28:03 +0000

    remove scipy dependency in ImageViewer, using PIL instead to resize

--- rosshow/src/librosshow/viewers/sensor_msgs/ImageViewer.py
+++ rosshow/src/librosshow/viewers/sensor_msgs/ImageViewer.py
@@ -1,6 +1,6 @@
-import numpy
+import numpy as np
 import time
-import scipy.misc
+import PIL.Image
 import librosshow.termgraphics as termgraphics
 
 class ImageViewer(object):
@@ -31,24 +31,26 @@ class ImageViewer(object):
         w = self.g.shape[0]
         h = self.g.shape[1]
         if self.image.encoding == 'bgr8':
-            current_image = numpy.frombuffer(self.image.data, numpy.uint8).reshape((self.image.height, self.image.width, 3))[:, :, ::-1]
+            current_image = np.frombuffer(self.image.data, np.uint8).reshape((self.image.height, self.image.width, 3))[:, :, ::-1]
         elif self.image.encoding == 'rgb8':
-            current_image = numpy.frombuffer(self.image.data, numpy.uint8).reshape((self.image.height, self.image.width, 3))
+            current_image = np.frombuffer(self.image.data, np.uint8).reshape((self.image.height, self.image.width, 3))
         elif self.image.encoding == 'mono8' or self.image.encoding == '8UC1':
-            current_image = numpy.frombuffer(self.image.data, numpy.uint8).reshape((self.image.height, self.image.width))
-            current_image = numpy.array((current_image.T, current_image.T, current_image.T)).T
+            current_image = np.frombuffer(self.image.data, np.uint8).reshape((self.image.height, self.image.width))
+            current_image = np.array((current_image.T, current_image.T, current_image.T)).T
         elif self.image.encoding == 'mono16' or self.image.encoding == '16UC1':
-            current_image = numpy.frombuffer(self.image.data, numpy.uint16).reshape((self.image.height, self.image.width)).astype(numpy.float)
-            current_image_max = numpy.percentile(current_image, 95)
-            current_image_min = numpy.percentile(current_image, 5)
+            current_image = np.frombuffer(self.image.data, np.uint16).reshape((self.image.height, self.image.width)).astype(np.float)
+            current_image_max = np.percentile(current_image, 95)
+            current_image_min = np.percentile(current_image, 5)
             current_image = 255*((current_image - current_image_min)/(current_image_max - current_image_min))
-            current_image = numpy.clip(current_image, 0, 255) #.astype(numpy.uint8)
-            current_image = numpy.array((current_image.T, current_image.T, current_image.T)).T
+            current_image = np.clip(current_image, 0, 255) #.astype(np.uint8)
+            current_image = np.array((current_image.T, current_image.T, current_image.T)).T
         else:
             print("Image encoding " + self.image.encoding + " not supported yet.")
             return
 
-        image_ratio = 0.5 * float(current_image.shape[0]) / current_image.shape[1] # height / width
+        current_image_obj = PIL.Image.fromarray(current_image.astype(np.uint8))
+
+        image_ratio = 0.5 * float(current_image_obj.size[1]) / current_image_obj.size[0] # height / width
         terminal_ratio = 0.5 * float(h) / w  # height / width
 
         if image_ratio > terminal_ratio:
@@ -58,8 +60,8 @@ class ImageViewer(object):
            target_image_width = int(w / 2.0)
            target_image_height = int(image_ratio * target_image_width)
 
-        resized_image = list(map(tuple, scipy.misc.imresize(current_image, \
-                (target_image_height, target_image_width)).reshape((target_image_width * target_image_height, 3))))
+        resized_image_obj = current_image_obj.resize((target_image_width, target_image_height), PIL.Image.BILINEAR)
+        resized_image = np.fromstring(resized_image_obj.tobytes(), dtype = np.uint8).reshape(target_image_width, target_image_height, 3)
 
         self.g.image(resized_image, target_image_width, target_image_height, (0, 0), image_type = termgraphics.IMAGE_RGB_2X4)
 
