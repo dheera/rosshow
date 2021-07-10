9b8c43517cb60433f3a66ec2197535fe4f4f9b0c
dheera <dheera@dheera.net>
2021-07-10 22:56:08 +0000

    Revert "comment"
    This reverts commit 6c61faf5ca022a2ba6d247c0106acb8ffaaec1ba.

--- librosshow/rosshow.py
+++ librosshow/rosshow.py
@@ -5,7 +5,7 @@ import os
 try:
     import rospy # ROS1
 except ImportError:
-    import librosshow.rospy2 as rospy # ROS2
+    import librosshow.rospy2 as rospy # ROS2, run as module
 except ModuleNotFoundError as e:
     print(str(e))
     exit(1)
