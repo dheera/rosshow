301ddd2d57b68a9e8c6a6008e55a91223377eed7
dheera <dheera@dheera.net>
2021-07-10 23:16:58 +0000

    preliminary ROS2 compatibility

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
-package_name = 'librosshow'
+package_name = 'rosshow'
 
 setup(
-    name='package_name',
+    name=package_name,
     version='2.0.0',
     packages=find_packages(),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=[
         'numpy',
         'pillow',
@@ -16,4 +19,9 @@ setup(
     description='rosshow: Visualize ROS topics in a terminal with ASCII art',
     license='bsd.threeclause',
     tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "rosshow = rosshow.rosshow:main",
+        ],
+    },
 )
