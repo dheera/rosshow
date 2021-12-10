ac76cd9a262e607bf23158eadc61e33ce41a5a0f
charlielito <candres.alv@gmail.com>
2021-12-10 15:11:01 -0500

    Fix typo in comment

--- rosshow/viewers/sensor_msgs/PointCloud2Viewer.py
+++ rosshow/viewers/sensor_msgs/PointCloud2Viewer.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 try:
     import sensor_msgs.point_cloud2 as pcl2
-except ImportError:  # ins ros2 sensor_msgs.point_cloud2 is not available
+except ImportError:  # in ros2 sensor_msgs.point_cloud2 is not available
     import rosshow.viewers.sensor_msgs.ros2_pointcloud2 as pcl2
 
 import rosshow.termgraphics as termgraphics
